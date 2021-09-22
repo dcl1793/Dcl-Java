71a5a7121a6d9e2a72d4c81c6b5411e8002d5371
dachuanlong <dcl@123456>
2021-09-22 20:20:42 +0800

    feat: json格式化

--- src/main/java/hs/dcl/test/controller/JsonController.java
+++ src/main/java/hs/dcl/test/controller/JsonController.java
@@ -31,22 +31,22 @@ public class JsonController {
     private final Logger logger = LoggerFactory.getLogger(JsonController.class.getSimpleName());
 
     /**
+     * @return ResultBody
      * @Author dcl
      * @Description json解析测试1
      * @Date 2021/9/22 20:07
      * @Param
-     * @return
      */
     @RequestMapping(value = "/test1", method = RequestMethod.GET)
-    public ResultBody test1()  {
-        try{
+    public ResultBody test1() {
+        try {
             //发送http请求给中转服务
             String url = "http:127.0.0.1:8303/cbs/ferry/equip/app/appCorpDevList";
             HttpHeaders headers = new HttpHeaders();
-            headers.add("head","head");
+            headers.add("head", "head");
             HashMap<String, Object> params = new HashMap<>();
             params.put("corpId", "1");
-            HttpEntity entity = new HttpEntity<>(params,headers);
+            HttpEntity entity = new HttpEntity<>(params, headers);
             RestTemplate restTemplate = new RestTemplate();
             ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
             String body = exchange.getBody();
@@ -56,75 +56,72 @@ public class JsonController {
             String error_no = jsonObj.getString("status");
             if ("OK".equals(error_no)) {
                 String data = jsonObj.getString("data");
-                if(StringUtils.isEmpty(data)){
-                    return  ResultBody.success();
-                }else {
+                if (StringUtils.isEmpty(data)) {
+                    return ResultBody.success();
+                } else {
                     //json是个array
-                    List<Object> result =  JSONArray.parseArray(data,Object.class);
+                    List<Object> result = JSONArray.parseArray(data, Object.class);
                     return ResultBody.success(result);
                 }
             } else {
                 String errorMsg = jsonObj.getString("message");
-                logger.error("调用530侧获取设备列表接口失败："+errorMsg);
-                throw new BaseBizException("调用5X0侧获取设备列表接口失败："+errorMsg);
+                logger.error("调用530侧获取设备列表接口失败：" + errorMsg);
+                throw new BaseBizException("调用5X0侧获取设备列表接口失败：" + errorMsg);
             }
         } catch (Exception e) {
             logger.error("GA网侧通过摆渡服务拿到设备列表失败: ", e);
-            throw new BaseBizException("内部错误："+e.getMessage());
+            throw new BaseBizException("内部错误：" + e.getMessage());
         }
     }
 
 
-
     /**
+     * @return ResultBody
      * @Author dcl
      * @Description json解析测试2
      * @Date 2021/9/22 20:08
      * @Param
-     * @return
      */
     @RequestMapping(value = "/test2", method = RequestMethod.GET)
     public ResultBody test2() throws Exception {
 
-            //发送http请求给中转服务
-            String url = "http:127.0.0.1:8303/cbs/ferry/equip/app/appCorpDevList";
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("head","head");
-            HashMap<String, Object> params = new HashMap<>();
-            params.put("corpId", "1");
-            HttpEntity entity = new HttpEntity<>(params,headers);
-            RestTemplate restTemplate = new RestTemplate();
-            String body;
-            try{
-                ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
-                body = exchange.getBody();
-                logger.debug("查询设备缩略图url:{}, 返回值:{}", url, body);
-            }catch (Exception e){
-                logger.error("向ZK服务,请求获取设备缩略图失败:", e);
-                throw new Exception("向ZK服务,请求获取设备缩略图失败:" + e.toString());
-            }
-
-            logger.debug("app查询设备列表拿到的body:" + body);
-            //json是个对象
-            JsonResultBody<GraphDTO> response = JSONObject.parseObject(body,
-                    new TypeReference<JsonResultBody<GraphDTO>>() {
-                    });
-
-            JsonResultBody<List<GraphDTO>> responseList = JSONObject.parseObject(body,
-                    new TypeReference<JsonResultBody<List<GraphDTO>>>() {
-                    });
-
-            if (null == response) {
-                throw new Exception("ZK服务,提供的设备列表数据格式异常: 无法正常解析");
-            }
-
-            if (response.getSuccess() == null || !response.getSuccess()) {
-                logger.warn("查询设备缩略图失败, errorCode:{}, errorMsg:{}", response.getErrorCode(), response.getErrorMsg());
-                throw new Exception("查询设备缩略图失败, 原因:" + response.getErrorMsg());
-            }
-
-            GraphDTO data = response.getData();
-            return ResultBody.success(data);
+        //发送http请求给中转服务
+        String url = "http:127.0.0.1:8303/cbs/ferry/equip/app/appCorpDevList";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("head", "head");
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("corpId", "1");
+        HttpEntity entity = new HttpEntity<>(params, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String body;
+        try {
+            ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+            body = exchange.getBody();
+            logger.debug("查询设备缩略图url:{}, 返回值:{}", url, body);
+        } catch (Exception e) {
+            logger.error("向ZK服务,请求获取设备缩略图失败:", e);
+            throw new Exception("向ZK服务,请求获取设备缩略图失败:" + e.toString());
         }
+
+        logger.debug("app查询设备列表拿到的body:" + body);
+        //json是个对象
+        JsonResultBody<GraphDTO> response = JSONObject.parseObject(body,
+                new TypeReference<JsonResultBody<GraphDTO>>() {
+                });
+
+        JsonResultBody<List<GraphDTO>> responseList = JSONObject.parseObject(body,
+                new TypeReference<JsonResultBody<List<GraphDTO>>>() {
+                });
+
+        if (null == response) {
+            throw new Exception("ZK服务,提供的设备列表数据格式异常: 无法正常解析");
+        }
+
+        if (response.getSuccess() == null || !response.getSuccess()) {
+            logger.warn("查询设备缩略图失败, errorCode:{}, errorMsg:{}", response.getErrorCode(), response.getErrorMsg());
+            throw new Exception("查询设备缩略图失败, 原因:" + response.getErrorMsg());
+        }
+        GraphDTO data = response.getData();
+        return ResultBody.success(data);
     }
 }
